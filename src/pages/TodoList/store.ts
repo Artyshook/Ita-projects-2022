6d967553c11530761970955e1b3905b94e6906cd
Artyshook <saibel@email.cz>
2022-09-19 12:19:20 +0200

    bugs fix

--- src/pages/TodoList/store.ts
+++ src/pages/TodoList/store.ts
@@ -3,15 +3,14 @@ import { legacy_createStore } from 'redux'
 import { v1 } from 'uuid'
 export type Task = { id: string; task: string; isDone: boolean }
 
-export const loadState = (key: string): Task[] | [] => {
+export const loadState = (key: string) => {
   try {
     const serializedState = localStorage.getItem(key)
 
     if (serializedState === null) {
       return []
     }
-
-    return JSON.parse(serializedState)
+    return JSON.parse(serializedState) as []
   } catch (err) {
     return []
   }
@@ -21,9 +20,13 @@ export const saveState = (key: string, state: Task[]) => {
   try {
     const serializedState = JSON.stringify(state)
     localStorage.setItem('state', serializedState)
-  } catch (err) {
-    return err
+  } catch (error) {
+    console.error
   }
+}
+
+const setPersist = (value: Task[]) => {
+  saveState('state', value)
 }
 
 const initialState: Task[] = loadState('state')
@@ -31,25 +34,25 @@ export const tasksReducer = (state = initialState, action: ActionTypes): Task[] 
   switch (action.type) {
     case 'ADD-TASK':
       const addTaskState = [...state, { id: v1(), task: action.title, isDone: false }]
-      saveState('state', addTaskState)
+      setPersist(addTaskState)
       return addTaskState
     case 'REMOVE-TASK':
       const filterTaskState = state.filter(task => task.id !== action.id)
-      saveState('state', filterTaskState)
+      setPersist(filterTaskState)
       return filterTaskState
 
     case 'CHANGE-STATUS':
       const changeStatusState = state.map(task =>
         task.id === action.id ? { ...task, isDone: !action.status } : task
       )
-      saveState('state', changeStatusState)
+      setPersist(changeStatusState)
       return changeStatusState
 
     case 'CHANGE-ORDER':
       const dragItem = state[action.start]
       state.splice(action.start, 1)
       state.splice(action.end, 0, dragItem)
-      saveState('state', state)
+      setPersist(state)
       return [...state]
 
     default:
@@ -65,45 +68,40 @@ type ActionTypes =
   | changeOrderACType
 
 type addTaskACType = ReturnType<typeof addTaskAC>
-export const addTaskAC = (title: string) => {
-  return {
+export const addTaskAC = (title: string) =>
+  ({
     type: 'ADD-TASK',
     title,
-  } as const
-}
+  } as const)
 type removeTaskACType = ReturnType<typeof removeTaskAC>
-export const removeTaskAC = (id: string) => {
-  return {
+export const removeTaskAC = (id: string) =>
+  ({
     type: 'REMOVE-TASK',
     id,
-  } as const
-}
+  } as const)
 
 type changeTaskStatusACType = ReturnType<typeof changeTaskStatusAC>
-export const changeTaskStatusAC = (id: string, status: boolean) => {
-  return {
+export const changeTaskStatusAC = (id: string, status: boolean) =>
+  ({
     type: 'CHANGE-STATUS',
     id,
     status,
-  } as const
-}
+  } as const)
 
 type changeFilterACType = ReturnType<typeof changeFilterAC>
-export const changeFilterAC = (filter: FilterValuesType) => {
-  return {
+export const changeFilterAC = (filter: FilterValuesType) =>
+  ({
     type: 'CHANGE-FILTER',
     filter,
-  } as const
-}
+  } as const)
 
 type changeOrderACType = ReturnType<typeof changeOrderAC>
-export const changeOrderAC = (start: number, end: number) => {
-  return {
+export const changeOrderAC = (start: number, end: number) =>
+  ({
     type: 'CHANGE-ORDER',
     start,
     end,
-  } as const
-}
+  } as const)
 
 export const store = legacy_createStore(tasksReducer)
 export type AppRootStateType = ReturnType<typeof tasksReducer>
